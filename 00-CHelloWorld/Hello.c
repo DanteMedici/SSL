57beb2ceec79665d344ba93b400d471a40c16f24
Dante Medici <dmedici@frba.utn.edu.ar>
2025-03-31 20:03:46 -0300

    Update Hello.c

--- 00-CHelloWorld/Hello.c
+++ 00-CHelloWorld/Hello.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
+/* HolaMundo.c  
+ * Programa que imprime "¡Hola, mundo!" en la consola.  
+ * Dante Medici
+ * 20250331
+ */
 
-/* Imprime "¡Hola, mundo!"*/
+#include <stdio.h>
 
 int main () {
     printf("¡Hola, mundo!\n");
